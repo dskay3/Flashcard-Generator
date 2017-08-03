1f0fb1ef20f88593d42927860fa71af02c661e3e
Daniel <dskay3@gmail.com>
2017-08-02 21:49:13 -0400

    Added constructors and updated package.json to include NPM chalk.

---
+++ BasicCard.js
@@ -0,0 +1,10 @@
+// BasicCard constructor
+var BasicCard = function (front, back) {
+    // Text on the front card
+    this.front = front;
+
+    // Text on the back card
+    this.back = back;
+}
+
+module.exports = BasicCard;
---
+++ ClozeCard.js
@@ -0,0 +1,31 @@
+// NPM installs
+const chalk = require('chalk');
+
+// ClozeCard constructor
+var ClozeCard = function (text, cloze) {
+    // Contains the full text
+    this.fullText = text;
+
+    // Contains the cloze-deleted portion of the text
+    this.cloze = cloze;
+
+    // Contains the partial text
+    this.partial(this.fullText, this.cloze);
+
+    // Checks to see if there is a cloze in the text
+    this.clozeChecker(this.fullText, this.cloze);
+}
+
+// Partial property that only contains the partial text
+ClozeCard.prototype.partial = function(text, cloze) {
+    this.partial = (text).replace(cloze, '...');
+}
+
+// Throws an error if a cloze is not present in the text
+ClozeCard.prototype.clozeChecker = function(text, cloze) {
+    if (!text.includes(cloze)) {
+        console.log(chalk.red("Error: Cloze is not present in the text."));
+    }
+}
+
+module.exports = ClozeCard;
